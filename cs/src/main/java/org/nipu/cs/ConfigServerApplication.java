88c35b1c6debc82bf5b8483272752fd3c49702df
Daria_Sovkova <Daria_Sovkova@epam.com>
2020-02-18 17:28:08 +0300

    eureka + zuul + config-server

--- cs/src/main/java/org/nipu/cs/ConfigServerApplication.java
+++ cs/src/main/java/org/nipu/cs/ConfigServerApplication.java
@@ -1,14 +1,16 @@
-package org.nipu.pc;
+package org.nipu.cs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableConfigServer
 @EnableEurekaClient
-public class PcApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PcApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
--- zuul/src/main/java/org/nipu/zuul/ZuulApplication.java
+++ zuul/src/main/java/org/nipu/zuul/ZuulApplication.java
@@ -1,14 +1,14 @@
-package org.nipu.pc;
+package org.nipu.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
-@EnableEurekaClient
-public class PcApplication {
+public class ZuulApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PcApplication.class, args);
+        SpringApplication.run(ZuulApplication.class, args);
     }
 }
